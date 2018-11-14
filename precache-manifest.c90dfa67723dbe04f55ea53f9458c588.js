2fada4772c46d4385c4cf173abc92f3b2847b933
Steven <skoch@tripadvisor.com>
2018-11-14 18:47:27 +0000

    Updates

---
+++ precache-manifest.c90dfa67723dbe04f55ea53f9458c588.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc61d57071ea793bd54f",
+    "url": "/static/js/main.bc61d570.chunk.js"
+  },
+  {
+    "revision": "771d009e12941703f509",
+    "url": "/static/js/1.771d009e.chunk.js"
+  },
+  {
+    "revision": "bc61d57071ea793bd54f",
+    "url": "/static/css/main.0120a1a9.chunk.css"
+  },
+  {
+    "revision": "c5185d5c2c748ee324257f0b74703aef",
+    "url": "/index.html"
+  }
+];
